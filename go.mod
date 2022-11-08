824c4f85636c9f00bb218eedb3f94af6eaa5fd77
Mithat Can Velioğlu <mithatcanvelioglu@Mithats-MacBook-Pro-2.local>
2022-11-09 00:06:26 +0300

    tests added

--- go.mod
+++ go.mod
@@ -4,14 +4,20 @@ go 1.18
 
 require (
 	github.com/MitoVeli/math_grpc_client v0.0.0-20221108195614-dc77a9f48907
+	github.com/stretchr/testify v1.8.1
+	github.com/test-go/testify v1.1.4
 	google.golang.org/grpc v1.50.1
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
 	google.golang.org/genproto v0.0.0-20221107162902-2d387536bcdd // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ test/math_operations_service_test.go
@@ -0,0 +1,63 @@
+package test
+
+import (
+	"testing"
+
+	mathOperations "github.com/MitoVeli/math_grpc_server/pkg/math"
+	"github.com/test-go/testify/assert"
+)
+
+func TestDoMath(t *testing.T) {
+
+	firstNumber := int64(10)
+	secondNumber := int64(5)
+	var result int64
+
+	mathOperationsService := mathOperations.NewMathOperationsService()
+
+	t.Run("Add", func(t *testing.T) {
+
+		err := mathOperationsService.DoMath(firstNumber, secondNumber, "+", &result)
+
+		assert.Equal(t, result, firstNumber+secondNumber)
+		assert.Nil(t, err)
+	})
+
+	t.Run("Subtract", func(t *testing.T) {
+
+		err := mathOperationsService.DoMath(firstNumber, secondNumber, "-", &result)
+
+		assert.Equal(t, result, firstNumber-secondNumber)
+		assert.Nil(t, err)
+	})
+
+	t.Run("Multiply", func(t *testing.T) {
+
+		err := mathOperationsService.DoMath(firstNumber, secondNumber, "*", &result)
+
+		assert.Equal(t, result, firstNumber*secondNumber)
+		assert.Nil(t, err)
+	})
+
+	t.Run("Divide", func(t *testing.T) {
+
+		err := mathOperationsService.DoMath(firstNumber, secondNumber, "/", &result)
+
+		assert.Equal(t, result, firstNumber/secondNumber)
+		assert.Nil(t, err)
+	})
+
+	t.Run("Divide by zero", func(t *testing.T) {
+
+		err := mathOperationsService.DoMath(0, 0, "/", &result)
+
+		assert.Equal(t, "cannot divide by zero", err.Error())
+	})
+
+	t.Run("Bad operation operator", func(t *testing.T) {
+
+		err := mathOperationsService.DoMath(firstNumber, secondNumber, "&", &result)
+
+		assert.Equal(t, "invalid operation sign, operation sign:&", err.Error())
+	})
+}
